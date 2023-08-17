0cf17b1a0c1dab7fc242ebeca0893638cb733d51
Chi Nguyen <chi.nguyen@live.com>
2023-08-18 01:12:34 +0300

    feartures/categories - Add middlerware

---
+++ backend/src/Presentation/BookVoyage.WebApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using System.Text.Json;
+using BookVoyage.Application.Common.Exceptions;
+
+namespace BookVoyage.WebApi.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment hostEnvironment)
+    {
+        _next = next;
+        _logger = logger;
+        _hostEnvironment = hostEnvironment;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var response = _hostEnvironment.IsDevelopment()
+                ? new AppException(context.Response.StatusCode, e.Message, e.StackTrace?.ToString())
+                : new AppException(context.Response.StatusCode, e.Message, "Internal Server Error");
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
